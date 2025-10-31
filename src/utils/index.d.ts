dda93b17e5e2cafaf3f145ff8dab852a5dd101de
scx1332 <sieciech@gmail.com>
2025-10-31 22:09:37 +0100

    Changes

--- src/utils/index.d.ts
+++ src/utils/index.d.ts
@@ -1,5 +1,3 @@
-import { Utils } from './utils'
-export { Utils } from './utils'
-export { Svg } from './svg'
-export default Utils
-//# sourceMappingURL=index.d.ts.map
+import { Utils } from './utils';
+export { Utils } from './utils';
+export default Utils;
