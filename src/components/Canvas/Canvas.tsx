ae6a04bf871f400b1746aa84356643606c3b57db
scx1332 <sieciech@gmail.com>
2025-10-31 21:53:10 +0100

    Core code

---
+++ src/components/Canvas/Canvas.tsx
@@ -0,0 +1,134 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { observer } from 'mobx-react-lite'
+import * as React from 'react'
+import {
+  usePreventNavigationCss,
+  useZoomEvents,
+  useSafariFocusOutFix,
+  useCanvasEvents,
+  useCameraCss,
+  useKeyEvents,
+  usePerformanceCss,
+} from '~hooks'
+import type {
+  TLAssets,
+  TLBinding,
+  TLBounds,
+  TLPage,
+  TLPageState,
+  TLPerformanceMode,
+  TLShape,
+  TLSnapLine,
+  TLUsers,
+} from '~types'
+import { Brush } from '~components/Brush'
+import { Page } from '~components/Page'
+import { Users } from '~components/Users'
+import { useResizeObserver } from '~hooks/useResizeObserver'
+import { inputs } from '~inputs'
+import { UsersIndicators } from '~components/UsersIndicators'
+import { SnapLines } from '~components/SnapLines/SnapLines'
+import { Grid } from '~components/Grid'
+import { Overlay } from '~components/Overlay'
+
+interface CanvasProps<T extends TLShape, M extends Record<string, unknown>> {
+  page: TLPage<T, TLBinding>
+  pageState: TLPageState
+  assets: TLAssets
+  snapLines?: TLSnapLine[]
+  grid?: number
+  users?: TLUsers<T>
+  userId?: string
+  hideBounds: boolean
+  hideHandles: boolean
+  hideIndicators: boolean
+  hideBindingHandles: boolean
+  hideCloneHandles: boolean
+  hideResizeHandles: boolean
+  hideRotateHandle: boolean
+  hideGrid: boolean
+  externalContainerRef?: React.RefObject<HTMLElement>
+  performanceMode?: TLPerformanceMode
+  meta?: M
+  id?: string
+  onBoundsChange: (bounds: TLBounds) => void
+}
+
+export const Canvas = observer(function _Canvas<
+  T extends TLShape,
+  M extends Record<string, unknown>
+>({
+  id,
+  page,
+  pageState,
+  assets,
+  snapLines,
+  grid,
+  users,
+  userId,
+  meta,
+  performanceMode,
+  externalContainerRef,
+  hideHandles,
+  hideBounds,
+  hideIndicators,
+  hideBindingHandles,
+  hideCloneHandles,
+  hideResizeHandles,
+  hideRotateHandle,
+  hideGrid,
+  onBoundsChange,
+}: CanvasProps<T, M>): JSX.Element {
+  const rCanvas = React.useRef<HTMLDivElement>(null)
+  const rContainer = React.useRef<HTMLDivElement>(null)
+  const rLayer = React.useRef<HTMLDivElement>(null)
+
+  inputs.zoom = pageState.camera.zoom
+
+  useResizeObserver(rCanvas, onBoundsChange)
+
+  useZoomEvents(pageState.camera.zoom, externalContainerRef || rCanvas)
+
+  useSafariFocusOutFix()
+
+  usePreventNavigationCss(rCanvas)
+
+  useCameraCss(rLayer, rContainer, pageState)
+
+  usePerformanceCss(performanceMode, rContainer)
+
+  useKeyEvents()
+
+  const events = useCanvasEvents()
+
+  return (
+    <div id={id} className="tl-container" ref={rContainer}>
+      <div id="canvas" className="tl-absolute tl-canvas" ref={rCanvas} {...events}>
+        {!hideGrid && grid && <Grid grid={grid} camera={pageState.camera} />}
+        <div ref={rLayer} className="tl-absolute tl-layer" data-testid="layer">
+          <Page
+            page={page}
+            pageState={pageState}
+            assets={assets}
+            hideBounds={hideBounds}
+            hideIndicators={hideIndicators}
+            hideHandles={hideHandles}
+            hideBindingHandles={hideBindingHandles}
+            hideCloneHandles={hideCloneHandles}
+            hideResizeHandles={hideResizeHandles}
+            hideRotateHandle={hideRotateHandle}
+            meta={meta}
+          />
+          {users && userId && (
+            <UsersIndicators userId={userId} users={users} page={page} meta={meta} />
+          )}
+          {pageState.brush && <Brush brush={pageState.brush} />}
+          {users && <Users userId={userId} users={users} />}
+        </div>
+        <Overlay camera={pageState.camera}>
+          {snapLines && <SnapLines snapLines={snapLines} />}
+        </Overlay>
+      </div>
+    </div>
+  )
+})
---
+++ src/hooks/useZoomEvents.ts
@@ -0,0 +1,124 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import * as React from 'react'
+import { useTLContext } from './useTLContext'
+import { Handler, useGesture, WebKitGestureEvent } from '@use-gesture/react'
+import { Vec } from '@scxdraw/vec'
+
+// Capture zoom gestures (pinches, wheels and pans)
+export function useZoomEvents<T extends HTMLElement>(zoom: number, ref: React.RefObject<T>) {
+  const rOriginPoint = React.useRef<number[] | undefined>(undefined)
+  const rPinchPoint = React.useRef<number[] | undefined>(undefined)
+  const rDelta = React.useRef<number[]>([0, 0])
+
+  const { inputs, bounds, callbacks } = useTLContext()
+
+  React.useEffect(() => {
+    const preventGesture = (event: TouchEvent) => event.preventDefault()
+    // @ts-ignore
+    document.addEventListener('gesturestart', preventGesture)
+    // @ts-ignore
+    document.addEventListener('gesturechange', preventGesture)
+    return () => {
+      // @ts-ignore
+      document.removeEventListener('gesturestart', preventGesture)
+      // @ts-ignore
+      document.removeEventListener('gesturechange', preventGesture)
+    }
+  }, [])
+
+  const handleWheel = React.useCallback<Handler<'wheel', WheelEvent>>(
+    ({ delta: wheelDelta, event: e }) => {
+      e.preventDefault()
+      // alt+scroll or ctrl+scroll = zoom
+      if ((e.altKey || e.ctrlKey) && e.buttons === 0) {
+        const point = inputs.pointer?.point ?? [bounds.width / 2, bounds.height / 2]
+        const info = inputs.pinch(point, point)
+        const delta = [...point, wheelDelta[1]]
+        callbacks.onZoom?.({ ...info, delta }, e)
+        return
+      }
+      // otherwise pan
+      const delta = e.shiftKey
+        ? // shift+scroll = pan horizontally
+          [wheelDelta[1], 0]
+        : // scroll = pan vertically (or in any direction on a trackpad)
+          [...wheelDelta]
+      if (inputs.isPinching) return
+      if (Vec.isEqual(delta, [0, 0])) return
+      const info = inputs.pan(delta, e)
+      callbacks.onPan?.(info, e)
+    },
+    [callbacks, inputs, bounds]
+  )
+
+  const handlePinchStart = React.useCallback<
+    Handler<'pinch', WheelEvent | PointerEvent | TouchEvent | WebKitGestureEvent>
+  >(
+    ({ origin, event }) => {
+      const elm = ref.current
+      if (!elm || !(event.target === elm || elm.contains(event.target as Node))) return
+      const info = inputs.pinch(origin, origin)
+      inputs.isPinching = true
+      callbacks.onPinchStart?.(info, event)
+      rPinchPoint.current = info.point
+      rOriginPoint.current = info.origin
+      rDelta.current = [0, 0]
+    },
+    [callbacks, inputs, bounds]
+  )
+
+  const handlePinch = React.useCallback<
+    Handler<'pinch', WheelEvent | PointerEvent | TouchEvent | WebKitGestureEvent>
+  >(
+    ({ origin, event, offset }) => {
+      const elm = ref.current
+      if (!(event.target === elm || elm?.contains(event.target as Node))) return
+      if (!rOriginPoint.current) return
+      const info = inputs.pinch(origin, rOriginPoint.current)
+      const trueDelta = Vec.sub(info.delta, rDelta.current)
+      rDelta.current = info.delta
+      callbacks.onPinch?.(
+        {
+          ...info,
+          point: info.point,
+          origin: rOriginPoint.current,
+          delta: [...trueDelta, offset[0]],
+        },
+        event
+      )
+      rPinchPoint.current = origin
+    },
+    [callbacks, inputs, bounds]
+  )
+
+  const handlePinchEnd = React.useCallback<
+    Handler<'pinch', WheelEvent | PointerEvent | TouchEvent | WebKitGestureEvent>
+  >(({ origin, event }) => {
+    const elm = ref.current
+    if (!(event.target === elm || elm?.contains(event.target as Node))) return
+    const info = inputs.pinch(origin, origin)
+    inputs.isPinching = false
+    callbacks.onPinchEnd?.(info, event)
+    rPinchPoint.current = undefined
+    rOriginPoint.current = undefined
+    rDelta.current = [0, 0]
+  }, [])
+
+  useGesture(
+    {
+      onWheel: handleWheel,
+      onPinchStart: handlePinchStart,
+      onPinch: handlePinch,
+      onPinchEnd: handlePinchEnd,
+    },
+    {
+      target: ref,
+      eventOptions: { passive: false },
+      pinch: {
+        from: zoom,
+        scaleBounds: () => ({ from: inputs.zoom, max: 5, min: 0.1 }),
+      },
+    }
+  )
+}
